2907d42cf4c90ad597e9bb5ac119b3a368a309e9
Dang Tien Ngoc <dangtienngoc@hotmail.com>
2018-04-07 21:45:26 +0700

    Task 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom"
+import './normalize.css';
 import './App.css';
+
+import Weather from './containers/todayWeatherController/Loadable';
+import Analysis from './containers/dataAnalysisController/Loadable';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="App-left">
+            <ul className="list-menu">
+              <li><Link to="/weather">Data Analysis</Link></li>
+              <li><Link to="/analysis">Today's weather</Link></li>
+            </ul>
+          </div>
+          <div className="App-right">
+            <Route exact path="/analysis" component={Analysis} />
+            <Route exact path="/weather" component={Weather} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
